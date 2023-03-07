1819a60508d7422900488cbe60e917937023a376
Carlos <carlosmanueldu@gmail.com>
2023-03-06 21:13:58 -0300

    Corrección de URL

--- src/components/FooterBar/MadeByFoot.vue
+++ src/components/FooterBar/MadeByFoot.vue
@@ -2,7 +2,7 @@
     <p class="text-muted m-0 pt-1 pl-3 text-left text-sm-right">
         <small>
             Made by
-            <a href="https://https://github.com/carlosmperilla" target="_blank">Carlos Perilla</a>
+            <a href="https://github.com/carlosmperilla" target="_blank">Carlos Perilla</a>
         </small>
     </p>
 </template>
